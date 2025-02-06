40e31ddc84487a8a97adef88a0fb223a7eb0d48d
IsipMervie <mervieisi29@gmail.com>
2025-02-06 12:48:07 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>MERVIE VIRAY ISIP</h1>
+      <div id="box">CPEITEL</div>
+    </div>
+  );
+}
+
+export default App;
